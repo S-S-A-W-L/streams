0ab154ece6baf15ab4a55ff87dbd29e66175b388
taylor <2747534980@qq.com>
2020-10-18 09:54:13 +0800

    update User.java file in the master branch

--- User.java
+++ User.java
@@ -1,6 +1,6 @@
 //在b1分支中创建文件
 public class User{
     private Integer id;
-    private String name;
-    private String className;
+    private String name;//在master分支中添加名称
+    private String className;//在master分支中添加班级名称
 }
